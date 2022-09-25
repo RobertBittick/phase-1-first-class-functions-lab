f6c32d392df715597f15308d7f060c19c376a355
Robert Bittick <robertbittick@gmail.com>
2022-09-24 21:30:25 -0700

    commit tree 5f1bd10d129ae186788e1c736e9cc5e2520dbc78

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    return ['Antonia', 'Nuru']
+}
+
+const returnLastTwoDrivers = function(){
+    return ['Amari','Mo']
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(integer) {
+    return function fareQuintupler() {
+        return integer * 5
+    }
+}
+
+const newvar = fareQuintupler()
+
+function fareDoubler(newvar){
+    return newvar * 2
+}
+function fareTripler(newvar){
+    return newvar * 3
+}
+
+const drivers = ['Antonia', 'Nuru','Amari','Mo']
+
+function selectDifferentDrivers(drivers,selectingDrivers){
+    if (selectingDrivers === returnFirstTwoDrivers) {
+        return drivers = ["Antonia", "Nuru"] 
+    } else if (returnLastTwoDrivers) {
+        return drivers = ["Amari", "Mo"]
+    }
+}
